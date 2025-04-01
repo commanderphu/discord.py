f6148f477f1b4ab6032fe1a3c3d7aad913aebc42
Joshua Phu <joshua@phuonline.de>
2025-04-02 00:31:12 +0200

    Füge Dockerfile, .gitignore und Hauptanwendungscode für Discord-Bot hinzu

---
+++ main.py
@@ -0,0 +1,45 @@
+import discord
+from dotenv import load_dotenv  # type: ignore
+import os
+
+import asyncio
+
+
+load_dotenv()  # Lädt die Variablen aus der .env-Datei
+# Discord API
+# Erstelle eine Discord-Client-Instanz
+# und lade die Umgebungsvariablen
+# aus der .env-Datei
+prefix = os.getenv("prefix")
+if not prefix:
+    raise ValueError("prefix ist nicht in der .env-Datei definiert!")
+
+class MyClient(discord.Client):  # Erbt von discord.Client
+    async def on_ready(self):  # Wird aufgerufen, wenn der Bot bereit ist
+          print(f'Logged in as {self.user}')  # Gibt den Namen des Bots aus
+    
+    async def on_message(self, message):  # Wird aufgerufen, wenn eine Nachricht gesendet wird
+        print(f'Message from {message.author}: {message.content}')  # Gibt die Nachricht und den Autor aus
+        if message.content.startswith(prefix + 'ping'):  # Wenn die Nachricht mit !ping beginnt
+            await message.channel.send('Pong!')
+    async def help(self, message):
+        if message.content.startswith(prefix + 'help'):
+            # Hier können Sie die Hilfe-Nachricht anpassen
+            await message.channel.send('Hier ist eine Liste der Befehle:\n!ping - Antwortet mit Pong!\n!help - Zeigt diese Nachricht an.')
+        # Hier können Sie weitere Befehle hinzufügen
+
+
+    
+intents = discord.Intents.default()  # Erstellt die Intents
+intents.messages = True  # Aktiviert die Nachrichten-Intents
+intents.message_content = True  # Aktiviert die Nachrichten-Inhalte-Intents
+
+    
+client = MyClient(intents=intents)  # Erstellt den Bot mit den Intents
+    
+    # Hole den Token aus der Umgebungsvariable
+token = os.getenv("bot_token")
+if not token:
+    raise ValueError("bot_token ist nicht in der .env-Datei definiert!")
+    
+client.run(token)  # Verwenden Sie start() statt run()
